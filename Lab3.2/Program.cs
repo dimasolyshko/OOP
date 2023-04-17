54d2c1b01eb1168e695a86ba40d6b8285f9e5444
dimasolyshko <dmitriy.solyshko@mail.ru>
2023-04-18 00:04:07 +0300

    lab3

---
+++ Lab3.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab3._2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = { 1, 2, 3 };
+            try
+            {
+                int result = numbers[4] / 0;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("DivideByZeroException: " + ex.Message);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("IndexOutOfRangeException: " + ex.Message);
+            }
+            try
+            {
+                int result = numbers[4] / 0;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("IndexOutOfRangeException: " + ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("DivideByZeroException: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -3;
+            const int b = 0;
+            int result = 1;
+            try
+            {
+                result = a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение! {ex.Message}");
+                try
+                {
+                    result = a / b;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Возникло повторное исключение! {ex.Message}");
+                }
+            }
+        }
+    }
+}
